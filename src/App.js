9d418967883a1c2539efe1e39675f877aa670a9b
DickensJuma <dickensjuma13@gmail.com>
2019-08-07 13:37:29 +0300

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import resume from './resume.json';
+import Experience from './Experience';
+import Project from './Project';
+import Skill from './Skill';
+import Education from './Education';
+import Language from './Language';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+
+  renderExperiences() {
+    let resultsArray = [];
+    resume.experiences.map((item, i) => {
+        resultsArray.push(<Experience item={item} key={i} />);
+    });
+    return resultsArray;
+  }
+
+  renderProjects() {
+    let resultsArray = [];
+    resume.projects.map((item, i) => {
+        resultsArray.push(<Project item={item} key={i} />);
+    });
+    return resultsArray;
+  }
+
+  renderSkills() {
+    let resultsArray = [];
+    resume.skills.map((item, i) => {
+        resultsArray.push(<Skill item={item} key={i} />);
+    });
+    return resultsArray;
+  }
+
+  renderEducations() {
+    let resultsArray = [];
+    resume.education.map((item, i) => {
+        resultsArray.push(<Education item={item} key={i} />);
+    });
+    return resultsArray;
+  }
+
+  renderLanguages() {
+    let resultsArray = [];
+    resume.languages.map((item, i) => {
+        resultsArray.push(<Language item={item} key={i} />);
+    });
+    return resultsArray;
+  }
+
+  renderInterests() {
+    let resultsArray = [];
+    resume.interests.map((item, i) => {
+        resultsArray.push(<li key={i}>{item}</li>);
+    });
+    return resultsArray;
+  }
+
+  render() {
+    // console.log(resume);
+    return (
+      <div className="wrapper">
+        <div className="sidebar-wrapper">
+            <div className="profile-container">
+                <img className="profile" src={resume.image} alt="" />
+                <h1 className="name">{resume.name}</h1>
+                <h3 className="tagline">Full Stack Developer</h3>
+            </div>
+            
+            <div className="contact-container container-block">
+                <ul className="list-unstyled contact-list">
+                    <li className="email"><i className="fa fa-envelope"></i><a href="mailto: yourname@email.com">{resume.email}</a></li>
+                    <li className="phone"><i className="fa fa-phone"></i><a href="tel:0123 456 789">{resume.phone}</a></li>
+                    <li className="website"><i className="fa fa-globe"></i><a href={resume.website} target="_blank">{resume.website}</a></li>
+                    <li className="twitter"><i className="fa fa-twitter"></i><a href={resume.twitterURL} target="_blank">{resume.twitter}</a></li>
+                </ul>
+            </div>
+            <div className="education-container container-block">
+                <h2 className="container-block-title">Education</h2>
+                {this.renderEducations()}
+            </div>
+            
+            <div className="languages-container container-block">
+                <h2 className="container-block-title">Languages</h2>
+                <ul className="list-unstyled interests-list">
+                    {this.renderLanguages()}
+                </ul>
+            </div>
+            
+            <div className="interests-container container-block">
+                <h2 className="container-block-title">Interests</h2>
+                <ul className="list-unstyled interests-list">
+                    {this.renderInterests()}
+                </ul>
+            </div>
+            
+        </div>
+        
+        <div className="main-wrapper">
+            
+            <section className="section summary-section">
+                <h2 className="section-title"><i className="fa fa-user"></i>Career Profile</h2>
+                <div className="summary">
+                    <p>I am a passionate Full-stack web development experience building virtual platforms for organizations at a professional level. My experience with developing software is primarily JavaScript and Node.js, but I am also comfortable using  HTML5, CSS3, and NoSQL databases, among others. I am creative and detail-oriented in designing platforms that are functional and user-friendly. I have a strong understanding of the use of SEO and social media in building brand awareness and gaining visibility in search engine rankings. I also value the use of mobile programming to design a well-rounded platform that caters to the unique needs of each customer I serve.</p>
+                </div>
+            </section>
+            
+            <section className="section experiences-section">
+                <h2 className="section-title"><i className="fa fa-briefcase"></i>Experiences</h2>
+
+                {this.renderExperiences()}
+                
+            </section>
+            
+            <section className="section projects-section">
+                <h2 className="section-title"><i className="fa fa-archive"></i>Projects</h2>
+                <div className="intro">
+                    <p>List of successfull and ongoing projects </p>
+                </div>
+
+                {this.renderProjects()}
+
+            </section>
+            
+            <section className="skills-section section">
+                <h2 className="section-title"><i className="fa fa-rocket"></i>Skills &amp; Proficiency</h2>
+                <div className="skillset"> 
+                    {this.renderSkills()}
+                    
+                </div>  
+            </section>
+            
+        </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
